f06bde2112c7f7a405b33190d3efcf7ba7dc3906
Vadim Shashenko <v.shashenko@gmail.com>
2015-09-28 15:34:52 +0200

    - Added a non-gtest mode.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "--no-gtest") == 0)
+    {
+      printf("Non-gtest mode.\n");
+      return 0;
+    }
+  }
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
